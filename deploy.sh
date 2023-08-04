a4dc52ba20226bcbc0dafb29ff21844133409101
Swapnil Shinde <swapnilshinde9382@gmail.com>
2023-08-04 23:23:31 +0530

    added env

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-zip -r archive.zip README.md app artisan bootstrap composer.json composer.lock config database lang package-lock.json package.json phpunit.xml postcss.config.js public resources routes storage stubs tailwind.config.js tests veer_co vendor vite.config.js veerctuv_veerco.sql
+zip -r archive.zip README.md app artisan bootstrap composer.json composer.lock config database lang package-lock.json package.json phpunit.xml postcss.config.js public resources routes storage stubs tailwind.config.js tests veer_co vendor vite.config.js veerctuv_veerco.sql .env
 timestamp=$(date +%s)
 # mv archive.zip archive.zip
 scp archive.zip veerctuv@162.241.123.152:/home3/veerctuv/public_html
